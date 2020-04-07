b11367fd8cc7d37698f19d1191391af52922f14b
AAAA <dev@onerbs.com>
2020-04-07 01:13:54 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,42 @@
 const { src, dest, series, parallel } = require('gulp')
 
-const pug    = require('gulp-pug')
-const stylc  = require('gulp-stylus')
-const tsc    = require('gulp-typescript')
+const pug = require('gulp-pug')
+const cleaner = require('gulp-clean')
+const minifier = require('gulp-minify')
+const renamer = require('gulp-rename')
 
-const clean  = require('gulp-clean')
-const concat = require('gulp-concat')
-const minify = require('gulp-minify')
-const rename = require('gulp-rename')
-
-const pug_Build = () =>
+const build__pug = () =>
   src('src/index.pug')
     .pipe(pug())
-    .pipe(dest('docs'))
+    .pipe(dest('public'))
 
-const styl_Build = () =>
-  src('src/styl/age.styl')
-    .pipe(stylc({ compress: true }))
-    .pipe(dest('docs'))
+const job_minify = () =>
+  src('public/age.js')
+    .pipe(minifier({
+      mangle: {
+        eval: true,
+        toplevel: true
+      }
+    }))
+    .pipe(dest('public'))
 
-const styl_Rename = () =>
-  src("docs/age.css")
-    .pipe(rename("age.min.css"))
-    .pipe(dest("docs"))
+const job_suffix = () =>
+  src(`public/age-min.js`)
+    .pipe(renamer(`age.min.js`))
+    .pipe(dest('public'))
 
-const styl_Clean = () =>
-  src("docs/age.css", { read: false })
-    .pipe(clean())
+const job_clean = () =>
+  src([
+      'public/age.js', 
+      'public/age-min.js'
+  ], { read: false })
+    .pipe(cleaner())
 
-const ts_Build = () =>
-  src('src/ts/*.ts')
-    .pipe(tsc.createProject('tsconfig.json')())
-    .pipe(dest('dist'))
-
-const js_Concat = () =>
-  src('dist/*.js')
-    .pipe(concat('dist.js'))
-    .pipe(dest('dist'))
-
-const js_Minify = () =>
-  src('dist/dist.js')
-    .pipe(minify({ ext: { min: '.min.js' } }))
-    .pipe(dest('docs'))
-
-const js_Clean = () =>
-  src(["dist", "docs/dist.js"], { read: false })
-    .pipe(clean())
-
-exports.default = parallel(
-  series(pug_Build),
-  series(styl_Build, styl_Rename, styl_Clean),
-  series(ts_Build, js_Concat, js_Minify, js_Clean)
+exports.default = series(
+  parallel(
+    build__pug,
+    job_minify,
+  ),
+  job_suffix,
+  job_clean
 )
