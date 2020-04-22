2ed2ac6a7ac92ca0f4ebe24619b1653b5dce44fb
AAAA <dev@onerbs.com>
2020-04-22 02:50:28 -0500

    isolate theme

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,7 @@ import App from './App'
 import { Provider } from './lib/context'
 
 render (
-  <ThemeProvider theme={{
-    breakpoints: ["360px", "480px", "800px", "1024px"],
-    colors: {
-      text: "#ddd",
-      background: "#000",
-      shadow: "#000A",
-    },
-    radii: { default: "3px", plus: "5px" },
-    fontSizes: [ "2rem", "2.65rem", "3.35rem", "4rem", "1rem", "1.25rem", "1.55rem" ]
-  }}>
+  <ThemeProvider theme={theme}>
     <StrictMode>
       <Provider>
         <App/>
